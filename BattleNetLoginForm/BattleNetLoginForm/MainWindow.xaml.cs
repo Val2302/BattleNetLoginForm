64ef160f486519046d2d6ba38c91c687865c8d9f
Val2302 <42169262+Val2302@users.noreply.github.com>
2025-05-27 12:18:02 +0300

    Realized window buttons functional

--- BattleNetLoginForm/BattleNetLoginForm/MainWindow.xaml.cs
+++ BattleNetLoginForm/BattleNetLoginForm/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace BattleNetLoginForm
         {
             InitializeComponent( );
         }
+
+        private void CloseButton_MouseDown ( object sender, MouseButtonEventArgs e )
+        {
+            this.Close( );
+        }
+
+        private void MinButton_MouseDown ( object sender, MouseButtonEventArgs e )
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown ( object sender, MouseButtonEventArgs e )
+        {
+            if ( e.ChangedButton == MouseButton.Left )
+            {
+                this.DragMove( );
+            }
+        }
     }
 }
